af128c8ce65a3760b911c5851c5e59bd9d33e7f8
zhaoguo <leave>
2017-05-16 13:11:37 -0400

    got content one line.

--- baidu_tieba.py
+++ baidu_tieba.py
@@ -45,10 +45,45 @@ class BaiduTieba:
             return data.group(1).strip()
         else:
             return None
+
     def get_content(self, source):
+        # <div class="l_post j_l_post l_post_bright " data-field="
+        #   {"author":{"user_id":2822234658,
+        #   "user_name":"\u6e29\u67d4\u88ab\u8986\u706d",
+        #   "name_u":"%E6%B8%A9%E6%9F%94%E8%A2%AB%E8%A6%86%E7%81%AD&ie=utf-8",
+        #   "user_sex":2,"portrait":"22e2e6b8a9e69f94e8a2abe8a686e781ad37a8",
+        #   "is_like":1,"level_id":6,"level_name":"\u5ba0\u7269\u77e5\u5df1",
+        #   "cur_score":114,"bawu":0,"props":null},
+        #   "content":{"post_id":106027375899,"is_anonym"   **********
+        # <div class="user-hide-post-position"></div>
+        # <div class="d_author">
+        # <div class="d_post_content_main">
+        # <div class="p_content p_content p_content_nameplate">
+        # <div class="save_face_bg_hidden save_face_bg_0">
+        # <cc>
+            # <div id="post_content_106027375899"
+            # class="d_post_content j_d_post_content clearfix"> 四个月柯基耳朵有点发炎 导致那边耳朵不立怎么办。。</div>
+            # <br>
+        # </cc>
+        # patten = re.compile(r'<div class="l_post.*?data-field.*?user_name":"(.*?)","name_u.*?' +
+        #                     r'level_name":"(.*?)","cur_score.*?' +
+        #                     r'<div id="post_content_.*?">(.*?)</div', re.S)
+        # data = re.search(patten, source)
+        # if data:
+        #     print data.group(1), data.group(2), data.group(3)
+        #     return data.group(1).strip(), data.group(2).strip(), data.group(3).strip()
+        # else:
+        #     return None
 
 
-
+        patten = re.compile(r'level_name":"(.*?)".*?' +
+                            r'<div id="post_content_.*?">(.*?)</div', re.S)
+        data = re.search(patten, source)
+        if data:
+            print data.group(1), data.group(2)
+            return data.group(1).strip()
+        else:
+            return None
 
 if __name__ == '__main__':
     working_url = 'http://tieba.baidu.com/p/3824525683'
@@ -56,4 +91,6 @@ if __name__ == '__main__':
     page_source = baidu_tieba.get_page_source(1)
     baidu_tieba.get_page_title(page_source)
     baidu_tieba.get_page_num(page_source)
+    baidu_tieba.get_content(page_source)
+
 
